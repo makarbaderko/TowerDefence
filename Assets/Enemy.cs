973b06903e4d38803d823cfc7bb2021bb192af44
Makar Baderko <makarbaderko@gmail.com>
2020-12-28 12:35:46 +0300

    Enemies can move from start to end

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
     private Transform target;
-    private int waypointIndex = 0;
+    private int wavepointIndex = 0;
 
 
     void Start()
@@ -18,5 +18,20 @@ public class Enemy : MonoBehaviour
     {
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
     }
 }
